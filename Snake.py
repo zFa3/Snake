b5396226d44866ed9601b22c209e931c5991a720
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-04-25 14:34:27 -0400

    added collision detection and a gradual speed increase

--- Snake.py
+++ Snake.py
@@ -3,8 +3,6 @@
 # Python3 - Snake in Tkinter
 
 # TODO
-# add collision detection
-# add boundary detection
 # add variants
 # make snake go in diagonals?
 # make game over screen
@@ -16,17 +14,22 @@ import tkinter as tk, random as rd, time as tm
 SIDE_LEN = 600 # in pixels
 TILE_SIZE = 20 # in pixels
 LINE_WID = 1
-DELAY = 0.05 # in seconds
-PRNT_GRID = True
+DELAY = 0.1 # in seconds
+PRNT_GRID = False
+DEF_LEN = 40
+INVINCIBILITY_PERIOD = 3
+AREA = [i for i in range(SIDE_LEN)]
+toggle = False
 
 # important variables
 snake = []
 posX = posY = 18
 snake_head = [(SIDE_LEN)//2 + 10, (SIDE_LEN)//2 + 10]
 apple = [posX * TILE_SIZE + 10, posY * TILE_SIZE + 10]
-snake_length = 4
+snake_length = DEF_LEN
 gameRunning = True
 # all directions snake can go in
+# right, left, down, up
 directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 # index in directions list
 dir = 2 
@@ -35,12 +38,13 @@ dir = 2
 root = tk.Tk()
 root.geometry(f"{SIDE_LEN}x{SIDE_LEN}")
 game_canvas = tk.Canvas(root)
+game_canvas.config(background="#838383")
 
 def draw():
     global game_canvas
     game_canvas.config(width=SIDE_LEN, height=SIDE_LEN)
     game_canvas.delete("all")
-    game_canvas.create_text(SIDE_LEN//2, SIDE_LEN//2, text = str(snake_length - 1), font=("Courier new", 350, "bold"), fill="#D0D0D0")
+    game_canvas.create_text(SIDE_LEN//2, SIDE_LEN//2, text = str(snake_length - 1), font=("Courier new", 350, "bold"), fill="#C0FFEE")
     if PRNT_GRID:
         for line in range(SIDE_LEN//TILE_SIZE):
             # iterates creating the vertical lines
@@ -50,30 +54,33 @@ def draw():
             game_canvas.create_line(0, line * TILE_SIZE, SIDE_LEN, line*TILE_SIZE, width = LINE_WID)
     for index, item in enumerate(snake):
         ind_row, ind_col = item[0], item[1]
-        game_canvas.create_rectangle(ind_row - 10, ind_col - 10, (ind_row + TILE_SIZE//2), (ind_col + TILE_SIZE//2), fill=(f"#{(hex(0)[2:]):02}{hex(max(255 - index * 2, 0))[2:]:02}{hex(0)[2:]:02}"))
-    game_canvas.create_rectangle(apple[0] - 10, apple[1] - 10, (apple[0] + TILE_SIZE//2), (apple[1] + TILE_SIZE//2), fill="red")
+        game_canvas.create_rectangle(ind_row - 10, ind_col - 10, (ind_row + TILE_SIZE//2), (ind_col + TILE_SIZE//2), fill=(f"#{(hex(0)[2:]):02}{hex(max(int(255 * (index/len(snake))), 0))[2:]:02}{hex(0)[2:]:02}"), outline="")
+    game_canvas.create_rectangle(apple[0] - 10, apple[1] - 10, (apple[0] + TILE_SIZE//2), (apple[1] + TILE_SIZE//2), fill="red", outline="")
     game_canvas.update()
     game_canvas.pack()
 
 def keypress(event):
-    global dir
-    keycodes = {
-        68:0,
-        87:3,
-        65:1,
-        83:2
-    }
+    global dir, toggle
+    keycodes = {68:0,87:3,65:1,83:2,37:1,38:3,39:0,40:2}
+    invalids = {0:1, 1:0, 2:3, 3:2}
     try:
-        dir = keycodes[event.keycode]
+        if dir != invalids[keycodes[event.keycode]] and toggle:
+            dir = keycodes[event.keycode]
+            toggle = False
     except: pass
 def main():
+    global gameRunning, DELAY
     root.bind("w", keypress)
     root.bind("a", keypress)
     root.bind("s", keypress)
     root.bind("d", keypress)
-    global snake_head, apple, snake_length
+    root.bind("<Down>", keypress)
+    root.bind("<Up>", keypress)
+    root.bind("<Right>", keypress)
+    root.bind("<Left>", keypress)
+    global snake_head, apple, snake_length, toggle
     tm.sleep(DELAY*2)
-    while gameRunning:
+    for i in range(INVINCIBILITY_PERIOD):
         draw()
         tm.sleep(DELAY)
         snake_head[0] += directions[dir][0] * TILE_SIZE
@@ -86,5 +93,26 @@ def main():
             posX, posY = rd.randint(0, 29), rd.randint(0, 29)
             apple = [posX * TILE_SIZE + 10, posY * TILE_SIZE + 10]
             snake_length += 1
-main()
-root.mainloop()
+        toggle = True
+    while gameRunning:
+        toggle = True
+        draw()
+        tm.sleep(DELAY)
+        snake_head[0] += directions[dir][0] * TILE_SIZE
+        snake_head[1] += directions[dir][1] * TILE_SIZE
+        #print(snake)
+        if len(snake) >= snake_length:
+            snake.pop(0)
+        if snake[-1][0] == apple[0] and snake[-1][1] == apple[1]:
+            posX, posY = rd.randint(0, 29), rd.randint(0, 29)
+            apple = [posX * TILE_SIZE + 10, posY * TILE_SIZE + 10]
+            snake_length += 1
+            DELAY *= 0.99
+            DELAY = max(0.05, DELAY)
+            print(DELAY)
+        if snake_head in snake or not snake_head[0] in AREA or not snake_head[1] in AREA:
+            gameRunning = False
+        snake.append(snake_head[:])
+tm.sleep(1); main()
+# mainloop isnt neccessary?
+# root.mainloop()
